244352b9dae013d5d8f28e29004de1834978d440
Niraj Rajput <98304477+nirajrajput-dev@users.noreply.github.com>
2025-12-15 22:03:47 +0530

    correct underline spacing

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
             href="https://dddigitalsolution.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-muted-foreground hover:text-primary transition-colors duration-300 underline decoration-dashed decoration-muted-foreground underline-offset-6"
+            className="text-muted-foreground hover:text-primary transition-colors duration-300 underline decoration-dashed decoration-muted-foreground underline-offset-8"
           >
             DD Solutions
           </a>
