8ab975512fc269b533df01785e58f48a57d359a9
Niraj Rajput <nirajrajput.dev@gmail.com>
2025-12-31 21:29:34 +0530

    added Lenis smooth scroll

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,30 @@
 import { createRoot } from "react-dom/client";
+import { useEffect } from "react";
+import Lenis from "lenis";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+function Root() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <App />;
+}
+
+createRoot(document.getElementById("root")!).render(<Root />);
