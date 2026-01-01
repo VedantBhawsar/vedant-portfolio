d7857d253ed2c1daa5539b39675fba71c6cd0451
Niraj Rajput <nirajrajput.dev@gmail.com>
2026-01-01 20:27:32 +0530

    fix anchor tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
 
     if (isScrollLink(item)) {
       return (
-        
+        <a
           key={item.name}
           href={item.scrollTo!}
           onClick={(e) => {
